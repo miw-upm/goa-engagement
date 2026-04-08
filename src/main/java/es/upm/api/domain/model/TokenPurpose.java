284d04c925cf02ecbfcccf2c61ffd5312ed6bf7b
jiangzhe00110 <jz2000110@gmail.com>
2026-04-08 16:39:44 +0800

    add public access token generation for engagement letters

---
+++ src/main/java/es/upm/api/domain/model/TokenPurpose.java
@@ -0,0 +1,5 @@
+package es.upm.api.domain.model;
+
+public enum TokenPurpose {
+    ACCEPT_ENGAGEMENT
+}
---
+++ src/main/java/es/upm/api/infrastructure/mongodb/repositories/PublicAccessTokenRepository.java
@@ -0,0 +1,9 @@
+package es.upm.api.infrastructure.mongodb.repositories;
+
+import es.upm.api.infrastructure.mongodb.entities.PublicAccessTokenEntity;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.UUID;
+
+public interface PublicAccessTokenRepository extends MongoRepository<PublicAccessTokenEntity, UUID> {
+}
