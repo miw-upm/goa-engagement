f3cba0ac733a6362159378e6097d3f43fd9a55d4
SihuiXu <s.xu@alumnos.upm.es>
2026-04-08 21:45:07 +0200

    add changes for comment in EventService and EventResource miw-upm/goa#61

--- src/main/java/es/upm/api/domain/services/EventService.java
+++ src/main/java/es/upm/api/domain/services/EventService.java
@@ -1,8 +1,11 @@
 package es.upm.api.domain.services;
 
+import es.upm.api.domain.model.Comment;
 import es.upm.api.domain.persistence.EventPersistence;
-import org.springframework.stereotype.Service;
 import es.upm.api.domain.model.Event;
+import es.upm.api.domain.model.UserDto;
+import es.upm.api.domain.webclients.UserWebClient;
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -12,11 +15,14 @@ import java.util.UUID;
 public class EventService {
     private final EventPersistence eventPersistence;
     private final EngagementLetterService engagementLetterService;
+    private final UserWebClient userWebClient;
 
-    public EventService(EventPersistence eventPersistence, EngagementLetterService engagementLetterService) {
+    public EventService(EventPersistence eventPersistence,
+                        EngagementLetterService engagementLetterService,
+                        UserWebClient userWebClient) {
         this.eventPersistence = eventPersistence;
         this.engagementLetterService = engagementLetterService;
-
+        this.userWebClient = userWebClient;
     }
 
     public Event create(Event event) {
@@ -25,6 +31,16 @@ public class EventService {
         this.engagementLetterService.readById(event.getEngagementLetterId());
         this.eventPersistence.create(event);
         return event;
+    }
+
+    public Comment addComment(UUID eventId, String authenticatedUser, String content) {
+        UserDto author = this.userWebClient.readUserByMobile(authenticatedUser);
+        Comment comment = Comment.builder()
+                .author(author)
+                .createdDate(LocalDateTime.now())
+                .content(content)
+                .build();
+        return this.eventPersistence.addComment(eventId, comment);
     }
 
 }
--- src/main/java/es/upm/api/infrastructure/resources/EventResource.java
+++ src/main/java/es/upm/api/infrastructure/resources/EventResource.java
@@ -1,7 +1,10 @@
 package es.upm.api.infrastructure.resources;
 
+import es.upm.api.domain.model.Comment;
 import es.upm.api.domain.model.Event;
 import es.upm.api.domain.services.EventService;
+import es.upm.api.infrastructure.dtos.CommentCreateDto;
+import es.upm.api.infrastructure.dtos.EventCommentResponseDto;
 import es.upm.api.infrastructure.dtos.EventCreateDto;
 import es.upm.api.infrastructure.dtos.EventResponseDto;
 import es.upm.api.infrastructure.mappers.EventMapper;
@@ -9,13 +12,17 @@ import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @PreAuthorize(Security.ADMIN_MANAGER_OPERATOR)
 @RequestMapping(EventResource.EVENTS)
 public class EventResource {
     public static final String EVENTS = "/events";
+    public static final String ID_COMMENTS = "/{eventId}/comments";
 
     private final EventService eventService;
     private final EventMapper eventMapper;
@@ -32,5 +39,19 @@ public class EventResource {
         Event event = this.eventMapper.toEntity(eventCreateDto);
         Event createdEvent = this.eventService.create(event);
         return this.eventMapper.toDto(createdEvent);
+    }
+
+    @PostMapping(ID_COMMENTS)
+    @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Create comment for event")
+    public EventCommentResponseDto createComment(@PathVariable UUID eventId,
+                                                 @Valid @RequestBody CommentCreateDto commentCreateDto,
+                                                 Authentication authentication) {
+        Comment createdComment = this.eventService.addComment(
+                eventId,
+                authentication.getName(),
+                commentCreateDto.getContent()
+        );
+        return this.eventMapper.toEventCommentResponseDto(createdComment);
     }
 }
