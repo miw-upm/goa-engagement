f3ca59a0c3abff182c7fef5ff3026b4315ed5117
Vikas Kanswal <kwlvikas@gmail.com>
2026-04-12 15:43:28 +0200

    miw-upm/goa#50 - add alert creation endpoint

---
+++ src/main/java/es/upm/api/infrastructure/resources/AlertResource.java
@@ -0,0 +1,38 @@
+package es.upm.api.infrastructure.resources;
+
+import es.upm.api.domain.model.Alert;
+import es.upm.api.domain.services.AlertService;
+import es.upm.api.infrastructure.dtos.AlertCreateDto;
+import es.upm.api.infrastructure.dtos.AlertResponseDto;
+import es.upm.api.infrastructure.mappers.AlertMapper;
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@PreAuthorize(Security.ADMIN_MANAGER_OPERATOR)
+@RequestMapping(AlertResource.ALERTS)
+public class AlertResource {
+    public static final String ALERTS = "/alerts";
+
+    private final AlertService alertService;
+    private final AlertMapper alertMapper;
+
+    public AlertResource(AlertService alertService, AlertMapper alertMapper) {
+        this.alertService = alertService;
+        this.alertMapper = alertMapper;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Create alert")
+    public AlertResponseDto create(@Valid @RequestBody AlertCreateDto alertCreateDto,
+                                   Authentication authentication) {
+        Alert alert = this.alertMapper.toEntity(alertCreateDto);
+        Alert createdAlert = this.alertService.create(alert, authentication.getName());
+        return this.alertMapper.toDto(createdAlert);
+    }
+}
