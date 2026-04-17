eec841bc6807eb9f7ea10012d56869c81189cf9d
Vikas Kanswal <kwlvikas@gmail.com>
2026-04-17 18:35:48 +0200

    add mark alert notification as shown endpoint and service logic miw-upm/goa#58

--- src/main/java/es/upm/api/infrastructure/resources/AlertNotificationResource.java
+++ src/main/java/es/upm/api/infrastructure/resources/AlertNotificationResource.java
@@ -8,11 +8,14 @@ import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @PreAuthorize(Security.ADMIN_MANAGER_OPERATOR)
@@ -36,5 +39,12 @@ public class AlertNotificationResource {
         return pendingNotifications.stream()
                 .map(this.alertMapper::toPendingNotificationDto)
                 .toList();
+    }
+
+    @PatchMapping("/{notificationId}/shown")
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Mark alert notification as shown")
+    public void markAsShown(@PathVariable UUID notificationId) {
+        this.alertService.markNotificationAsShown(notificationId);
     }
 }
