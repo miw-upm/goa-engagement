5ffcb715e8122bf990e03cc13147ebee85c23a60
JBernal <j.bernal@upm.es>
2026-05-03 07:21:56 +0200

    added email to sign. miw-upm/goa#153

--- src/main/java/es/upm/api/adapter/out/email/feign/EmailWriterAdapter.java
+++ src/main/java/es/upm/api/adapter/out/email/feign/EmailWriterAdapter.java
@@ -2,6 +2,7 @@ package es.upm.api.adapter.out.email.feign;
 
 import es.upm.api.domain.ports.out.email.EmailWriter;
 import es.upm.miw.mail.Email;
+import feign.form.FormData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -13,5 +14,11 @@ public class EmailWriterAdapter implements EmailWriter {
     @Override
     public void sendHtml(Email email) {
         this.goaSupportClient.sendHtml(email);
+    }
+
+    @Override
+    public void sendHtml(Email email, byte[] attachment, String fileName) {
+        FormData formData = new FormData("application/pdf", fileName, attachment);
+        goaSupportClient.sendHtml(email, formData);
     }
 }
