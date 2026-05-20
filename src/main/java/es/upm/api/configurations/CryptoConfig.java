7181037da517b0288b8c681c96318a0bb68faa5a
JBernal <j.bernal@upm.es>
2026-05-20 11:18:57 +0200

    removed  bean. miw-upm/goa#159

--- src/main/java/es/upm/api/configurations/CryptoConfig.java
+++ src/main/java/es/upm/api/configurations/CryptoConfig.java
@@ -5,18 +5,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.encrypt.BytesEncryptor;
 import org.springframework.security.crypto.encrypt.Encryptors;
-import org.springframework.security.crypto.encrypt.TextEncryptor;
 
 @Configuration
 public class CryptoConfig {
-
-    @Bean
-    public TextEncryptor textEncryptor(
-            @Value("${app.crypto.password}") String password,
-            @Value("${app.crypto.salt}") String salt
-    ) {
-        return Encryptors.delux(password, salt);
-    }
 
     @Bean
     public BytesEncryptor bytesEncryptor(
