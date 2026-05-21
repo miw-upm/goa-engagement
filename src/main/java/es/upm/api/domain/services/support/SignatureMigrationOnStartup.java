dcaa9f95c6d9846ad826fd2fcabc23f4fe25626c
JBernal <j.bernal@upm.es>
2026-05-21 14:15:34 +0200

    removed encrypt v1 migration

--- src/main/java/es/upm/api/domain/services/support/SignatureMigrationOnStartup.java
+++ src/main/java/es/upm/api/domain/services/support/SignatureMigrationOnStartup.java
@@ -20,7 +20,7 @@ import java.util.stream.Stream;
 
 @Service
 @Log4j2
-@Profile({"dev", "prod"})
+@Profile("NONE")
 public class SignatureMigrationOnStartup implements ApplicationRunner {
     private static final String LEGACY_PREFIX = "enc::";
     private final AdministrativeAuthorizationGateway administrativeAuthorizationGateway;
